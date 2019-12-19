2ac35f32f7f6932afcdd368863315c285d6a699b
Lakshmi Alavala <lakshmi.alavala@perficient.com>
2019-12-19 11:54:32 -0600

    new changes

--- src/main/java/com/example/customerAccount/Entity/DebitEntity.java
+++ src/main/java/com/example/customerAccount/Entity/DebitEntity.java
@@ -1,14 +1,12 @@
 package com.example.customerAccount.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name="Debit")
 public class DebitEntity
 {
     @Id
